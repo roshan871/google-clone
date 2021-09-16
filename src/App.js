eecf4b64665cd30c8546476f79b9d8d79c1db6b3
roshan Dahal <roshandahal747@gmail.com>
2021-09-15 20:41:45 -0600

    Added router to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-
+import Home from './Pages/Home'
+import { HashRouter as Router, Switch, Route} from "react-router-dom";
 function App() {
   return (
     //BEM
     <div className="App">
-    <h1>Google clone</h1>
+    <Router>
+      <Switch>
+          <Route path='/search'>
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }

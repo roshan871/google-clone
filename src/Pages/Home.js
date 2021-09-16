a8bcdd9d206219855f63dfffb41fe3cbed98b462
roshan Dahal <roshandahal747@gmail.com>
2021-09-15 20:42:50 -0600

    Added link to header

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,23 @@ import { Avatar } from '@material-ui/core';
 function Home() {
     return (
         <div className='home'>
-           <h1>Home page</h1>
+         <div className='home__header'>
+                <div className='home__headerLeft'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+
+                </div>
+                <div className='home__headerRight'>
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
+                    <AppsIcon />
+                    <Avatar />
+                    
+                    {/* Avatart */}
+                    {/* Icon */}
+                </div>
+            </div>
+            <div className='home__body'></div>
         </div>
     )
 }

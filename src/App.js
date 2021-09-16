32f5021ee059433d71f3d97d130afbe20c4cea9a
roshan Dahal <roshandahal747@gmail.com>
2021-09-15 20:24:57 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
-import Home from './Pages/Home'
-import { HashRouter as Router, Switch, Route} from "react-router-dom";
+
 function App() {
   return (
     //BEM
     <div className="App">
-    <Router>
-      <Switch>
-          <Route path='/search'>
-            <h1>This is the search page</h1>
-          </Route>
-          <Route path='/'>
-            <Home />
-          </Route>
-      </Switch>
-      
-      </Router>
+    <h1>Google clone</h1>
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,23 +6,7 @@ import { Avatar } from '@material-ui/core';
 function Home() {
     return (
         <div className='home'>
-            <div className='home__header'>
-                <div className='home__header__left'>
-                    <Link to='/about'>About</Link>
-                    <Link to='/store'>Store</Link>
-
-                </div>
-                <div className='home__header__right'>
-                    <Link to='/gmail'>Gmail</Link>
-                    <Link to='/images'>Images</Link>
-                    <AppsIcon />
-                    <Avatar />
-                    
-                    {/* Avatart */}
-                    {/* Icon */}
-                </div>
-            </div>
-            <div className='home__body'></div>
+           <h1>Home page</h1>
         </div>
     )
 }
